44b4fb766103760425c47720f10ad9f0e442ba7f
klas9802 <klas9806@gmail.com>
2024-03-02 22:14:05 +0800

    0302 update v2

---
+++ Lab0225_InitProject/Lab0225_InitProject/Controllers/AiRecommendController.cs
@@ -0,0 +1,45 @@
+﻿using Azure;
+using Lab0225_InitProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Linq;
+
+namespace Lab0225_InitProject.Controllers
+{
+    public class AiRecommendController : Controller
+    {
+        private readonly TravelContext _context;
+
+        public AiRecommendController(TravelContext context)
+        {
+            _context = context;
+        }
+
+        
+        public async Task<IActionResult> Index()
+        {
+            //問題: 一個index用了2個partivalView這兩個View各自用了不同的資料表 應該怎麼處裡?
+            var tagLists = await _context.TagList.ToListAsync();
+            var spots = await _context.Spots.ToListAsync();
+            var cityList = await _context.UniqueCity.ToListAsync();
+
+            ArrayList myAL = new ArrayList();
+            myAL.Add(tagLists);
+            myAL.Add(spots);
+            myAL.Add(cityList);
+            return View(myAL.ToArray());
+        }
+
+        [HttpPost]
+        public List<Spots> Itinerary(string city)
+        {
+            var query = _context.Spots.Where(x => x.city == city);
+            return query.ToList();
+        }
+
+    }
+
+    
+}
+
